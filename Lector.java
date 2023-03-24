f6586667c24ff239daafdf0f3371573cd3ceb521
Diego <dgv3105@gmail.com>
2023-03-24 11:32:37 -0600

    Código versión 3.2.1

--- Lector.java
+++ Lector.java
@@ -42,18 +42,4 @@ public class Lector {
             logger.severe(e.getMessage());
         }
     }
-
-    public void loadDictionary(String fileName, BinarySearchTree<String, Association<String, String>> englishBST, BinarySearchTree<String, Association<String, String>> spanishBST, BinarySearchTree<String, Association<String, String>> frenchBST) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String[] parts = line.split(",");
-            Association<String, String> association = new Association<String, String>(parts[0], parts[1], parts[2]);
-            englishBST.put(parts[0].toLowerCase(), association);
-            spanishBST.put(parts[1].toLowerCase(), association);
-            frenchBST.put(parts[2].toLowerCase(), association);
-        }
-        reader.close(); 
-    }
-    
 }
